05377b8146c64cc8d64f0b702508d6dab93f79a6
ersahii <mehraj95he@gmail.com>
2022-09-11 11:33:50 +0530

    javascript coding challenge

---
+++ codingchallenge1.js
@@ -0,0 +1,3 @@
+// calculation of BMI(Body mass index) which is calculated by using formaula
+//  BMI = mass / height square or mass / height * height
+// mass should be in kilograms and height in meters.
